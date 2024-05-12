7b8051b08a3f9016c1b42c0940254f5ba857fc7c
robertonunes <jrvn@cin.ufpe.br>
2024-05-12 04:26:56 -0300

    fix: Rota /recursos/get funcionando para retornar todos os recursos de cada sala

--- backend/tests/step_definitions/adicionarRecurso.test.js
+++ backend/tests/step_definitions/adicionarRecurso.test.js
@@ -1,5 +1,4 @@
 const { defineFeature, loadFeature } = require("jest-cucumber");
-const {Then} = require("@cucumber/cucumber");
 
 const feature = loadFeature('adicionarRecurso.feature');
 
@@ -7,28 +6,30 @@ defineFeature(feature, test => {
     test('Adicionar recurso a uma sala', ({Given, When, Then, pending}) => {
         Given("que estou na pagina de {string}", (nomePagina) => {
                 pending();
-            }
-        )
+            });
+        Given("eu vejo {string} na lista de recursos da sala", () => {
+                pending();
+            });
+        Given("eu não vejo {string} na lista de recursos da sala", () => {
+            pending();
+        });
         When("eu escolho {string}", () => {
                 pending();
-            }
-        )
+            });
         When("eu preencho o campo {string} com {string}", () => {
                 pending();
-            }
-        )
+            });
         When("eu escolho {string}", () => {
                 pending();
-            }
-        )
+            });
         Then("eu vejo a mensagem {string}", () => {
                 pending();
-            }
-        )
+            });
         Then("eu vejo o recurso {string} na lista de recursos da sala", () => {
                 pending();
-            }
-        )
-
+            });
+        Then("eu vejo o recurso {string} na lista de recursos da sala apenas uma vez", () => {
+                pending();
+            });
     });
 });
