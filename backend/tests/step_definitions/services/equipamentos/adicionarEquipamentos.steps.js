76fe21b231c55ab8f2838e335aa47f81c2d3006c
Roberto Nunes <jrvn@cin.ufpe.br>
2024-06-06 19:09:07 -0300

    refactor(adicionarEquipamentos.steps.js): Create steps class to group steps for reuse

--- backend/tests/step_definitions/services/equipamentos/adicionarEquipamentos.steps.js
+++ backend/tests/step_definitions/services/equipamentos/adicionarEquipamentos.steps.js
@@ -2,6 +2,8 @@ const jc = require('jest-cucumber');
 const loadFeature = jc.loadFeature;
 const defineFeature = jc.defineFeature;
 const fs = require('fs');
+const request = require('supertest');
+const app = require('../../../../app');
 
 const feature = loadFeature('tests/features/equipamentos/adicionarEquipamento.feature');
 let equipamentos = []
@@ -37,115 +39,119 @@ defineFeature(feature, (test) => {
     });
 
     //Steps to reuse
-    //Given steps
-    const givenNotEquipmentExist = (given) => {
-        given(/^não existe o equipamento "(.*)" com "(.*)" "(.*)"$/, async (nome, campo, identificador) => {
-            expect(equipmentExists(equipamentos, nome, campo, identificador)).not.toBe(true);
-        });
-    };
-    const givenEquipmentExist = (given) => {
-        given(/^existe o equipamento "(.*)" com "(.*)" "(.*)"$/, async (nome, campo, identificador) => {
-            expect(equipmentExists(equipamentos, nome, campo, identificador)).toBe(true);
-        });
-    };
-    const givenRequest = (given) => {
-        given(/^eu recebo uma requisição "(.*)" do usuario "(.*)" logado como "(.*)"$/, async (nome, campo, valor) => {
-            expect(nome).toBe('POST');
-        });
-    }
-    //When steps
-    const whenRequest = (when) => {
-        when(/^eu recebo uma requisição "(.*)" do usuario "(.*)" logado como "(.*)"$/, async (req) => {
-            expect(req).toBe('/POST');
-        });
-    };
-    const whenverifyEquipment = (when) => {
-        when(/^os dados são verificados como "(.*)" "(.*)"$/, (nome, valor) => {
-            expect(nome).toBe(valor);
-        });
-    }
-    //Then steps
-    const thenPatrimonioIsOnDatabase = (then) => {
-        then(/^o equipamento (.*) com patrimonio (.*) está no banco de dados$/, async (nome, patrimonio) => {
-            expect(equipamentos).toContainEqual({nome: nome, patrimonio: patrimonio});
-        });
-    };
-    const thenResponseError = (then) => {
-        then(/^eu envio uma resposta de "(.*)" com codigo "(.*)"$/), async (type, code) => {
-            expect(type).toBe('error');
-            expect(code).toBe('404');
+    class steps  {
+        //Given steps
+        static givenNotEquipmentExist = (given) => {
+            given(/^não existe o equipamento "(.*)" com "(.*)" "(.*)"$/, async (nome, campo, identificador) => {
+                expect(equipmentExists(equipamentos, nome, campo, identificador)).not.toBe(true);
+            });
+        };
+        static givenEquipmentExist = (given) => {
+            given(/^existe o equipamento "(.*)" com "(.*)" "(.*)"$/, async (nome, campo, identificador) => {
+                expect(equipmentExists(equipamentos, nome, campo, identificador)).toBe(true);
+            });
+        };
+        static givenRequest = (given) => {
+            given(/^eu recebo uma requisição "(.*)" do usuario "(.*)" logado como "(.*)"$/, async (nome, campo, valor) => {
+                expect(nome).toBe('POST');
+            });
+        };
+        //When steps
+        static whenRequest = (when) => {
+            when(/^eu recebo uma requisição "(.*)" do usuario "(.*)" logado como "(.*)"$/, async (req) => {
+                expect(req).toBe('/POST');
+            });
+        };
+        static whenverifyEquipment = (when) => {
+            when(/^os dados são verificados como "(.*)" "(.*)"$/, (nome, valor) => {
+                expect(nome).toBe(valor);
+            });
         }
-    };
-    const thenSerialNumbersAreOnDatabase = (then) => {
-        then(/^os equipamentos (.*) com numeros de serie (\d+) estão no banco de dados$/, async (numeros) => {
-            expect(equipamentos).toContainEqual({serial: numeros});
-        });
-    };
-    const andMessageError = (and, message) => {
-        and(/^mensagem "(.*)"$/, async (mensagem) => {
-            expect(mensagem).toBe(message);
-        });
-    }
-    //And steps
-    const andFieldMatch = (and) => {
-        and(/^(.*) "(.*)"$/, async (equipamento, campo, valor) => {
-            expect(equipamento[campo]).toBe(valor);
-        });
-    };
-    const andFieldEmpty = (and) => {
-        and(/^(.*) "(.*)"$/, async (equipamento, campo) => {
-            expect(equipamento[campo]).toBe('');
-        });
-    };
-    const andReqIsBatch = (and) => {
-        and(/^a requisição possui uma "(.*)"$/, async (campo) => {
-            expect(campo).toBe('inserção em lote');
-        });
-    };
-    const andReqIsNotBatch = (and) => {
-        and(/^a requisição possui uma "(.*)"$/, async (campo) => {
-            expect(campo).toBe('inserção unica');
-        });
-    };
-    const andVerifySerialNumbers = (and) => {
-        and(/^os numeros de serie (\d+)$/, async (numeros) => {
+        //Then steps
+        static thenPatrimonioIsOnDatabase = (then) => {
+            then(/^o equipamento (.*) com patrimonio (.*) está no banco de dados$/, async (nome, patrimonio) => {
+                expect(equipamentos).toContainEqual({nome: nome, patrimonio: patrimonio});
+            });
+        };
+        static thenResponseError = (then) => {
+            then(/^eu envio uma resposta de "(.*)" com codigo "(.*)"$/), async (type, code) => {
+                expect(type).toBe('error');
+                expect(code).toBe('404');
+            }
+        };
+        static thenSerialNumbersAreOnDatabase = (then) => {
+            then(/^os equipamentos (.*) com numeros de serie (\d+) estão no banco de dados$/, async (numeros) => {
+                expect(equipamentos).toContainEqual({serial: numeros});
+            });
+        };
+        static andMessageError = (and, message) => {
+            and(/^mensagem "(.*)"$/, async (mensagem) => {
+                expect(mensagem).toBe(message);
+            });
+        }
+        //And steps
+        static andFieldMatch = (and) => {
+            and(/^(.*) "(.*)"$/, async (equipamento, campo, valor) => {
+                expect(equipamento[campo]).toBe(valor);
+            });
+        };
+        static andFieldEmpty = (and) => {
+            and(/^(.*) "(.*)"$/, async (equipamento, campo) => {
+                expect(equipamento[campo]).toBe('');
+            });
+        };
+        static andReqIsBatch = (and) => {
+            and(/^a requisição possui uma "(.*)"$/, async (campo) => {
+                expect(campo).toBe('inserção em lote');
+            });
+        };
+        static andReqIsNotBatch = (and) => {
+            and(/^a requisição possui uma "(.*)"$/, async (campo) => {
+                expect(campo).toBe('inserção unica');
+            });
+        };
+        static andVerifySerialNumbers = (and) => {
+            and(/^os numeros de serie (\d+)$/, async (numeros) => {
 
-        });
-    }
+            })
+        };
+
+    };
+
 
     //Scenarios tests
     test('Adicionando equipamento usando patrimonio com sucesso', ({given, when, then, and}) => {
-        givenNotEquipmentExist(given);
-        whenRequest(when);
-        andFieldMatch(and);
-        andFieldMatch(and);
-        andFieldMatch(and);
-        andFieldMatch(and);
-        andFieldMatch(and);
-        andFieldMatch(and);
-        thenPatrimonioIsOnDatabase(then);
+        steps.givenNotEquipmentExist(given);
+        steps.whenRequest(when);
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.thenPatrimonioIsOnDatabase(then);
     });
     test('Adicionando equipamento usando numero de serie com sucesso', ({given, when, then, and}) => {
-        givenNotEquipmentExist(given);
-        whenRequest(when);
-        andReqIsNotBatch(and);
-        andFieldMatch(and);
-        andFieldMatch(and);
-        andFieldMatch(and);
-        andFieldMatch(and);
-        andFieldMatch(and);
-        andFieldMatch(and);
-        thenPatrimonioIsOnDatabase(then);
+        steps.givenNotEquipmentExist(given);
+        steps.whenRequest(when);
+        steps.andReqIsNotBatch(and);
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.thenPatrimonioIsOnDatabase(then);
     });
     test('Adicionando equipamento duplicado', ({given, when, then, and}) => {
-        givenEquipmentExist(given);
-        whenRequest(when);
-        andFieldMatch(and);
-        andFieldMatch(and);
-        andFieldMatch(and);
-        andFieldMatch(and);
-        andFieldMatch(and);
-        andFieldMatch(and);
+        steps.givenEquipmentExist(given);
+        steps.whenRequest(when);
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
         then(/^eu envio uma resposta de "(.*)" com codigo "(.*)"$/, async (res, code) => {
             expect(res).toBe('erro');
             expect(code).toBe(404);
@@ -156,13 +162,13 @@ defineFeature(feature, (test) => {
 
     });
     test('Adicionando equipamento com nome vazio', ({given, when, then, and}) => {
-        givenRequest(given)
-        andFieldEmpty(and);
-        andFieldMatch(and);
-        andFieldMatch(and);
-        andFieldMatch(and);
-        andFieldMatch(and);
-        andFieldMatch(and);
+        steps.givenRequest(given)
+        steps.andFieldEmpty(and);
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
 
         then(/^eu envio uma resposta de "(.*)" com codigo "(.*)"$/, async (res, code) => {
             expect(res).toBe('erro');
@@ -198,60 +204,60 @@ defineFeature(feature, (test) => {
         });
     });
     test('Adicionando equipamento com estado de conservação vazio', ({given, when, then, and}) => {
-        givenRequest(given);
-        andFieldMatch(and);
-        andFieldMatch(and);
-        andFieldEmpty(and); //test to check if the field is empty
-        andFieldMatch(and);
-        andFieldMatch(and);
-        andFieldMatch(and);
-        thenResponseError(then);
-        andMessageError(and, 'Estado de conservação não pode ser vazio');
+        steps.givenRequest(given);
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.andFieldEmpty(and); //test to check if the field is empty
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.thenResponseError(then);
+        steps.andMessageError(and, 'Estado de conservação não pode ser vazio');
     });
     test('Adicionando equipamento com data de aquisição vazia', ({given, when, then, and}) => {
-        givenRequest(given);
-        andFieldMatch(and);
-        andFieldMatch(and);
-        andFieldMatch(and);
-        andFieldEmpty(and); //test to check if the field is empty
-        andFieldMatch(and);
-        andFieldMatch(and);
-        thenResponseError(then);
-        andMessageError(and, 'Data de aquisição não pode ser vazia');
+        steps.givenRequest(given);
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.andFieldEmpty(and); //test to check if the field is empty
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.thenResponseError(then);
+        steps.andMessageError(and, 'Data de aquisição não pode ser vazia');
     });
     test('Adicionando equipamento com valor estimado vazio', ({given, when, then, and}) => {
-        givenRequest(given);
-        andFieldMatch(and);
-        andFieldMatch(and);
-        andFieldEmpty(and); //test to check if the field is empty
-        andFieldMatch(and);
-        andFieldMatch(and);
-        andFieldMatch(and);
-        thenResponseError(then);
-        andMessageError(and, 'Valor estimado não pode ser vazio');
+        steps.givenRequest(given);
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.andFieldEmpty(and); //test to check if the field is empty
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.thenResponseError(then);
+        steps.andMessageError(and, 'Valor estimado não pode ser vazio');
     });
     test('Adicionando equipamento com estado de conservação não funcional', ({given, when, then, and}) => {
-        givenRequest(given);
-        andFieldMatch(and);
-        andFieldMatch(and);
-        andFieldMatch(and); //test to check if the field is empty
-        andFieldMatch(and);
-        andFieldMatch(and);
-        andFieldMatch(and);
-        thenResponseError(then);
-        andMessageError(and, 'Estado de conservação inválido');
+        steps.givenRequest(given);
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and); //test to check if the field is empty
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.thenResponseError(then);
+        steps.andMessageError(and, 'Estado de conservação inválido');
 
     });
     test('Adicionando equipamentos em lote por numero de serie', ({ given, and, when, then }) => {
-        givenRequest(given);
-        andReqIsBatch(and);
-        whenverifyEquipment(when);
-        andFieldMatch(and);
-        andFieldMatch(and);
-        andFieldMatch(and);
-        andFieldMatch(and);
-        andFieldMatch(and);
-        andVerifySerialNumbers(and);
-        thenSerialNumbersAreOnDatabase(then);
+        steps.givenRequest(given);
+        steps.andReqIsBatch(and);
+        steps.whenverifyEquipment(when);
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.andFieldMatch(and);
+        steps.andVerifySerialNumbers(and);
+        steps.thenSerialNumbersAreOnDatabase(then);
     });
 });
