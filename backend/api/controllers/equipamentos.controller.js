cd77b0ec904f2e7dbe81b3699d67aa8530ad756b
Roberto Nunes <jrvn@cin.ufpe.br>
2024-06-14 22:26:41 -0300

    Refactor(equipamentos.controller.js): Add equipamentos em lote em funcionamento

--- backend/api/controllers/equipamentos.controller.js
+++ backend/api/controllers/equipamentos.controller.js
@@ -106,41 +106,46 @@ exports.createSingleEquipment = (req, res) => {
 exports.createMultipleEquipments = (req, res) => {
     try{
         const equipments = equipamentosModel.loadDatabase();
-        const {nome, descricao, estado_conservacao, data_aquisicao, valor_estimado, quantidade, identificador} = req.body;
-        if (!nome || !descricao || !estado_conservacao || !data_aquisicao || !valor_estimado || !identificador) {
+        const {nome, descricao, estado_conservacao, data_aquisicao, valor_total_estimado, quantidade, identificador, equipamentos} = req.body;
+        if (!nome || !descricao || !estado_conservacao || !data_aquisicao || !valor_total_estimado || !identificador) {
             console.log(`POST /equipamentos/lote [400] BAD REQUEST`);
             res.status(400).send({message: 'Campos obrigatórios não preenchidos'});
             return;
         } else {
-            if(identificador.type === 'patrimonio') {
-                newEquipment = {nome, descricao, estado_conservacao, data_aquisicao, quantidade, valor_estimado};
+            if(identificador === 'patrimonio') {
+                newEquipment = {nome, descricao, estado_conservacao, data_aquisicao, quantidade, valor_total_estimado};
                 newEquipment.single = false;
                 equipments.forEach(equipment => {
                     if(equipment.single === false){
-                        identificador.value.forEach(pat => {
-                            if(equipment.patrimonio.find(patrimonio => pat === patrimonio)){
-                                console.log(`POST /equipamentos/lote [400] BAD REQUEST`);
-                                return res.status(400).send({message: 'Já existe um equipamento com este patrimonio'});
+                        equipment.equipamentos.forEach(eq => {
+                            if(eq.hasOwnProperty('patrimonio')){
+                                equipamentos.forEach(e => {
+                                    if(e.patrimonio === eq.patrimonio){
+                                        console.log(`POST /equipamentos/lote [400] BAD REQUEST`);
+                                        return res.status(400).send({message: 'Já existe um equipamento com este número de série'});
+                                    }
+                                });
+                                
                             }
                         });
                     }
                 });
-                newEquipment.patrimonio = identificador.value;
+                newEquipment.equipamentos = equipamentos;
                 equipments.push(newEquipment);
             } else if(identificador.type === 'numero_serie') {
-                newEquipment = {nome, descricao, estado_conservacao, data_aquisicao, quantidade, valor_estimado};
+                newEquipment = {nome, descricao, estado_conservacao, data_aquisicao, quantidade, valor_total_estimado};
                 newEquipment.single = false;
                 equipments.forEach(equipment => {
                     if(equipment.single === false){
                         identificador.value.forEach(numero_serie => {
-                            if(equipment.numero_serie.find(serie => numero_serie === serie)){
+                            if (equipment.hasOwnProperty('numero_serie') && (equipment.numero_serie.find(serie => numero_serie === serie))){
                                 console.log(`POST /equipamentos/lote [400] BAD REQUEST`);
                                 return res.status(400).send({message: 'Já existe um equipamento com este número de série'});
                             }
                         });
                     }
                 });
-                newEquipment.numero_serie = identificador.value;
+                
                 equipments.push(newEquipment);
             }
         }
