2aaa5bd91bf25b1a36df643c0a45a463655f473c
Roberto Nunes <jrvn@cin.ufpe.br>
2024-06-18 16:12:28 -0300

    chore: Update npm dependencies and remove unused files and code

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,19 +1,11 @@
 module.exports = {
-
-  preset: 'ts-jest',
-
   testEnvironment: 'node',
   moduleFileExtensions: ['js', 'json', 'ts'],
   rootDir: '',
   testRegex: '.steps.js$',
   transform: {
-  
-  '^.+\\.(t|j)s$': 'babel-jest',
-  
+    '^.+\\.(t|j)s$': 'babel-jest',
   },
-  
   setupFilesAfterEnv: ['./setupTests.js'],
-  
-
 };
 
