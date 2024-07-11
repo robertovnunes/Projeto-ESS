eb3262be959fe668160b21980ca13617d53f9d4a
Roberto Nunes <jrvn@cin.ufpe.br>
2024-07-11 14:13:29 -0300

    Refactor(equipamentos.controller.js): Correct return treatment from repository

--- backend/api/models/usuarios/baseModel.js
+++
@@ -1,9 +0,0 @@
-class BaseModel {
-    constructor(nome, login, senha) {
-        this.nome = nome;
-        this.login = login;
-        this.senha = senha;
-    }
-}
-
-module.exports = BaseModel;
