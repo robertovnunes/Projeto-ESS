5fe5c4aa158ce82362836e7b3fcc1cdbabd524c7
JJ <julnsilva20@gmail.com>
2024-07-21 23:16:15 -0300

    Fix: Consertar conflitos nos arquivos

--- backend/api/routes/login.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const loginController = require('../controllers/login.controller.js');
-
-module.exports = app => {
-    app.use('/usuarios', router);
-    router.post('/login', loginController.login);
-    router.delete('/logout', loginController.logout);
-}
--- backend/conf/server.js
+++ backend/conf/server.js
@@ -1,14 +1,8 @@
 const express = require('express');
-const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const port = 3001;
 
 const server = express();
 
-server.use(cors({
-    origin: 'http://localhost:3000', // Substitua pelo URL do seu frontend
-    credentials: true
-}));
 server.use(express.json())
 server.use(express.urlencoded({ extended: false }))
 
@@ -16,8 +10,10 @@ server.use(express.urlencoded({ extended: false }))
 server.use(cookieParser());
 
 ///ATENCAO
+/*
 server.listen(port, () => {
     console.log(`Server running on port ${port}`);
 }); 
+*/
 
 module.exports = server;
