dc1726d7882ba6a3c9ef2f99283507f28d4f3d8c
mariana <marianage75@gmail.com>
2024-07-23 14:48:37 -0300

    fix: imports

--- frontend/src/components/common/NavBarCin.js
+++ frontend/src/components/common/NavBarCin.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Cookie from 'js-cookie';
 import '../../style/NavBarCin.css';
-// import logo from '../../assets/CIn_logo.png'; 
-// import UserBox from './UserBox';
-// import '../../style/UserBox.css';
+import logo from '../../assets/CIn_logo.png'; 
+import UserBox from './UserBox';
+import '../../style/UserBox.css';
 const NavBarCin = ({ toggleUserBox, bgColor }) => {
   const initial = Cookie.get('nome')?.charAt(0).toUpperCase() || 'U';
 
   return (
     <nav className="navbar-cin">
         <div className="navbar-content-cin">
-            {/* <img src={logo} alt="Logo" className="logo-image-cin"/> */}
+            <img src={logo} alt="Logo" className="logo-image-cin"/>
             <span className="website-name-cin">Reservas CIn</span>
             <ul className="navbar-list-cin">
                 <li className="navbar-item-cin"><a href="/" className="navbar-link"><i className="fas fa-home"></i> Home</a></li>
