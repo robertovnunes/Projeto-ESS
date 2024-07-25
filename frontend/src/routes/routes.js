53e531473ae9662c87e9c8737046016acbcd99d7
Roberto Nunes <jrvn@cin.ufpe.br>
2024-07-25 14:51:23 +0000

    refactor: Update routes in routes.js file

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -4,20 +4,19 @@ import LoginPage from './pages/login';
 import MainPage from './pages/MainPage';
 import Home from './pages/Home';
 import Reservainicio from "../pages/reservaInicio";
-import Reservas from "../pages/reserva/reservaEquipamento"
-import UsersPage from './pages/administrador/usuarios'; // Página para Usuários
-import RoomsPage from './pages/administrador/salas'; // Página para Salas
-import SubjectsPage from './pages/administrador/disciplinas'; // Página para Disciplinas
-import EquipmentPage from './pages/administrador/equipamentos'; // Página para Equipamentos
-import EventsPage from './pages/administrador/eventos'; // Página para Eventos
+import UsersPage from '../pages/administrador/usuarios'; // Página para Usuários
+import RoomsPage from '../pages/administrador/salas'; // Página para Salas
+import SubjectsPage from '../pages/administrador/disciplinas'; // Página para Disciplinas
+import EquipmentPage from '../pages/administrador/equipamentos'; // Página para Equipamentos
+import EventsPage from '../pages/administrador/eventos'; // Página para Eventos
 
-import AlunosPage from './pages/administrador/alunosPage';
-import ProfessoresPage from './pages/administrador/professoresPage';
-import AdministradoresPage from './pages/administrador/administradoresPage';
+import AlunosPage from '../pages/administrador/alunosPage';
+import ProfessoresPage from '../pages/administrador/professoresPage';
+import AdministradoresPage from '../pages/administrador/administradoresPage';
 
-import AddAlunoPage from './pages/administrador/AddAlunoPage'; // Importe a nova página
-import AddProfessorPage from './pages/administrador/AddProfessorPage'
-import AddAdminPage from './pages/administrador/AddAdminPage'
+import AddAlunoPage from '../pages/administrador/AddAlunoPage'; // Importe a nova página
+import AddProfessorPage from '../pages/administrador/AddProfessorPage'
+import AddAdminPage from '../pages/administrador/AddAdminPage'
 
 const router = createBrowserRouter([
 
