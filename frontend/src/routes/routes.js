d8fb7ccf5b513f39471563b937bd39c4c70d1b99
Roberto Nunes <jrvn@cin.ufpe.br>
2024-07-25 07:43:34 -0300

    refactor: Update routes in routes.js file

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -1,13 +1,33 @@
 import {createBrowserRouter} from "react-router-dom";
-import App from "../App";
+
+import LoginPage from './pages/login';
+import MainPage from './pages/MainPage';
+import Home from './pages/Home';
 import Reservainicio from "../pages/reservaInicio";
 import Reservas from "../pages/reserva/reservaEquipamento"
+import UsersPage from './pages/administrador/usuarios'; // Página para Usuários
+import RoomsPage from './pages/administrador/salas'; // Página para Salas
+import SubjectsPage from './pages/administrador/disciplinas'; // Página para Disciplinas
+import EquipmentPage from './pages/administrador/equipamentos'; // Página para Equipamentos
+import EventsPage from './pages/administrador/eventos'; // Página para Eventos
+
+import AlunosPage from './pages/administrador/alunosPage';
+import ProfessoresPage from './pages/administrador/professoresPage';
+import AdministradoresPage from './pages/administrador/administradoresPage';
+
+import AddAlunoPage from './pages/administrador/AddAlunoPage'; // Importe a nova página
+import AddProfessorPage from './pages/administrador/AddProfessorPage'
+import AddAdminPage from './pages/administrador/AddAdminPage'
 
 const router = createBrowserRouter([
 
-    {path: "/", element: <App/>},
-    {path: "/reservaEquipamento", element: <Reservainicio/>},
-    {path: "/reservaEquipamento/equipamentos", element: <Reservas/>}
+    {path: "/", element: <Home />},
+    {path: "/reservaEquipamento", element: <Reservainicio />},
+    {path: "/reservaEquipamento/equipamentos", element: <Reservas />},
+    {path:"/login", element:<LoginPage />},
+    {path: "/mainpage", element: isAuthenticated ? <MainPage /> : <Navigate to="/login" />},
+
+
 
 ]);
 
