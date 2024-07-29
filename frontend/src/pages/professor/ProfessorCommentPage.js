fa5e6ff0f32265d8374f0e42fb8f206925e4fe02
Tomás Santos <tns@cin.ufpe.br>
2024-07-29 20:45:12 -0300

    testes de comentário professor e responder comentário no front funcionando agora

--- frontend/src/pages/professor/ProfessorCommentPage.js
+++ frontend/src/pages/professor/ProfessorCommentPage.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 const ProfessorCommentPage = () => {
   const [comment, setComment] = useState('');
   const [recipient, setRecipient] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
 
   const handleCommentChange = (event) => {
     setComment(event.target.value);
@@ -12,8 +14,26 @@ const ProfessorCommentPage = () => {
     setRecipient(event.target.value);
   };
 
-  const handleCommentSubmit = () => {
-    // Lógica para enviar o comentário
+  const handleCommentSubmit = async () => {
+
+    try {
+      const response = await axios.post('/comentarios', {
+        autor: 'aluno',
+        login: 'tns',
+        comentario: comment,
+        destinatario: recipient
+      });
+      
+      if (response.status === 201) {
+        setSuccessMessage('Comentário adicionado com sucesso');
+      } else {
+        setSuccessMessage('Erro ao adicionar comentário');
+      }
+    } catch (error) {
+      console.error('Erro ao enviar comentário:', error);
+      setSuccessMessage('Erro ao adicionar comentário');
+    }
+
     console.log('Comentário enviado:', { recipient, comment });
   };
 
@@ -32,6 +52,7 @@ const ProfessorCommentPage = () => {
         placeholder="Digite seu comentário aqui" 
       />
       <button onClick={handleCommentSubmit}>Enviar Comentário</button>
+      {successMessage && <p>{successMessage}</p>}
     </div>
   );
 };
