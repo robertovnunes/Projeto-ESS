27ab9b89461782f51105edfc4c50b91c612e5f20
JJ <julnsilva20@gmail.com>
2024-07-29 22:27:19 -0300

    Fix: Incluir uma dependência de pacote e adicionar o runAllTests

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -4,6 +4,7 @@ const cucumber = require('cypress-cucumber-preprocessor').default;
 module.exports = defineConfig({
   chromeWebSecurity: false,
   e2e: {
+    experimentalRunAllSpecs: true,
     setupNodeEvents(on, config) {
       on('file:preprocessor', cucumber());
       // implement node event listeners here
